19e0cbb717aefb8b6c2a339e9b678edb6bc1cc6b
231712600hengker <164556380+231712600hengker@users.noreply.github.com>
2026-07-03 17:06:53 +0000

    Add carousel photos

--- src/main.jsx
+++ src/main.jsx
@@ -57,17 +57,17 @@ const milestones = [
 
 const memories = [
   {
-    image: "",
-    title: "Foto favorit Dedek",
+    image: "Fav.jpeg",
+    title: "Foto favorit Abang",
     caption: "Yang satu ini nanti jadi alasan aku senyum sendiri.",
   },
   {
-    image: "",
-    title: "My 911 mode tenang",
+    image: "Cute.jpeg",
+    title: "My 911 mode nerd",
     caption: "Tempat aman kecil yang selalu bikin semuanya terasa mungkin.",
   },
   {
-    image: "",
+    image: "Luv.jpeg",
     title: "Trixy Theodora Situngkir",
     caption: "Nama lengkap yang selalu kedengaran cantik, bahkan saat lagi bahas revisi.",
   },
